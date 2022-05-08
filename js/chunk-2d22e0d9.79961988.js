5bd47bdffa981d04ff543af8f9367b33f067cf94
ythwfxy <1832164480@qq.com>
2022-05-08 11:05:38 +0800

    achieved

---
+++ js/chunk-2d22e0d9.79961988.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22e0d9"],{f9b2:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" 我是团购订单 ")])},c=[],l={},r=l,s=t("2877"),a=Object(s["a"])(r,u,c,!1,null,null,null);e["default"]=a.exports}}]);
